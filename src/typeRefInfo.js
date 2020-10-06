f7bd52786d8c44fe84f963287d0715bdde522efe
Vyacheslav <kosv2007@yandex.ru>
2020-10-06 22:33:59 +0500

    step 2

--- src/typeRefInfo.js
+++ src/typeRefInfo.js
@@ -3,11 +3,14 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const typeRefInfo = () => {
-  const { program } = require('commander');
+  const { Command } = require('commander');
+  const program = new Command();
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .helpOption('-h, --help', 'output usage information');
+    .helpOption('-h, --help', 'output usage information')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format');
 
   program.parse(process.argv);
 };
