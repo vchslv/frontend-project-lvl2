f8b62848c75b0767120de68cc0da7e393e6a4508
Vyacheslav <kosv2007@yandex.ru>
2020-10-13 21:37:51 +0500

    step 3. simple code

--- bin/gendiff.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import genDiff from '../src/index.js';
-
-genDiff();
---
+++ bin/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import { createRequire } from 'module';
+import genDiff from '../lib/gendiff.js';
+
+const require = createRequire(import.meta.url);
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filePath1> <filePath2>')
+  .action((filePath1, filePath2) => {
+    try {
+      const result = genDiff(filePath1, filePath2);
+      console.log(result);
+    } catch (err) {
+      console.log('File name or path error');
+    }
+  });
+
+program.parse(process.argv);
