8c8cb4505615347cc176c20c217360f13887457e
Vyacheslav <kosv2007@yandex.ru>
2020-11-05 07:46:11 +0500

    map -> flatMap

--- src/formatters/formatterPlain.js
+++ src/formatters/formatterPlain.js
@@ -8,33 +8,31 @@ const stringify = (value) => {
 };
 
 const runFormatToPlain = (innerTree, path = '') => {
-  const result = innerTree
-    .map((node) => {
-      if (node.type === 'nested') {
-        const additionalPath = `${path}${node.key}.`;
-        return runFormatToPlain(node.value, additionalPath);
-      }
-      if (node.type === 'removed') {
-        return `Property '${path}${node.key}' was removed`;
-      }
-      if (node.type === 'unchanged') {
-        return [];
-      }
-      if ((node.type === 'added') && (_.isObject(node.value))) {
-        return `Property '${path}${node.key}' was added with value: [complex value]`;
-      }
-      if (node.type === 'added') {
-        return `Property '${path}${node.key}' was added with value: ${stringify(node.value)}`;
-      }
-      if ((node.type === 'changed') && _.isObject(node.value1)) {
-        return `Property '${path}${node.key}' was updated. From [complex value] to ${stringify(node.value2)}`;
-      }
-      if ((node.type === 'changed') && _.isObject(node.value2)) {
-        return `Property '${path}${node.key}' was updated. From ${stringify(node.value1)} to [complex value]`;
-      }
-      return `Property '${path}${node.key}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
-    })
-    .filter((node) => (node.length > 0));
+  const result = innerTree.flatMap((node) => {
+    if (node.type === 'nested') {
+      const additionalPath = `${path}${node.key}.`;
+      return runFormatToPlain(node.value, additionalPath);
+    }
+    if (node.type === 'removed') {
+      return `Property '${path}${node.key}' was removed`;
+    }
+    if (node.type === 'unchanged') {
+      return [];
+    }
+    if ((node.type === 'added') && (_.isObject(node.value))) {
+      return `Property '${path}${node.key}' was added with value: [complex value]`;
+    }
+    if (node.type === 'added') {
+      return `Property '${path}${node.key}' was added with value: ${stringify(node.value)}`;
+    }
+    if ((node.type === 'changed') && _.isObject(node.value1)) {
+      return `Property '${path}${node.key}' was updated. From [complex value] to ${stringify(node.value2)}`;
+    }
+    if ((node.type === 'changed') && _.isObject(node.value2)) {
+      return `Property '${path}${node.key}' was updated. From ${stringify(node.value1)} to [complex value]`;
+    }
+    return `Property '${path}${node.key}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+  });
   return result.join('\n');
 };
 
