e267b25c24083b2dc97c6d7fd0c8ef61837b40fd
Vyacheslav <kosv2007@yandex.ru>
2020-11-06 01:03:11 +0500

    changed formatterplain

--- src/formatters/formatterPlain.js
+++ src/formatters/formatterPlain.js
@@ -1,17 +1,22 @@
 import _ from 'lodash';
 
-const isValueBoolleanOrNull = (value) => (typeof value === 'boolean') || (value === null);
-
 const stringify = (value) => {
-  const quote = isValueBoolleanOrNull(value) ? '' : '\'';
-  return `${quote}${value}${quote}`;
+  if (value === null) {
+    return value;
+  }
+  if (typeof value === 'object') {
+    return '[complex value]';
+  }
+  if (typeof value === 'boolean') {
+    return value;
+  }
+  return `'${value}'`;
 };
 
-const runFormatToPlain = (innerTree, path = '') => {
+const iter = (innerTree, path = '') => {
   const result = innerTree.flatMap((node) => {
     if (node.type === 'nested') {
-      const additionalPath = `${path}${node.key}.`;
-      return runFormatToPlain(node.children, additionalPath);
+      return iter(node.children, `${path}${node.key}.`);
     }
     if (node.type === 'removed') {
       return `Property '${path}${node.key}' was removed`;
@@ -19,23 +24,17 @@ const runFormatToPlain = (innerTree, path = '') => {
     if (node.type === 'unchanged') {
       return [];
     }
-    if ((node.type === 'added') && (_.isPlainObject(node.value))) {
-      return `Property '${path}${node.key}' was added with value: [complex value]`;
-    }
     if (node.type === 'added') {
       return `Property '${path}${node.key}' was added with value: ${stringify(node.value)}`;
     }
-    if ((node.type === 'changed') && _.isPlainObject(node.value1)) {
-      return `Property '${path}${node.key}' was updated. From [complex value] to ${stringify(node.value2)}`;
-    }
-    if ((node.type === 'changed') && _.isPlainObject(node.value2)) {
-      return `Property '${path}${node.key}' was updated. From ${stringify(node.value1)} to [complex value]`;
+    if (node.type === 'changed') {
+      return `Property '${path}${node.key}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
     }
     return `Property '${path}${node.key}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
   });
   return result.join('\n');
 };
 
-const formatToPlain = (innerTree) => runFormatToPlain(innerTree);
+const formatToPlain = (innerTree) => iter(innerTree);
 
 export default formatToPlain;
