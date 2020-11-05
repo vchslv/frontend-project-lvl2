98993a0058fc7de4a2df5a13867c416f78cba2c2
vchslv <kosv2007@yandex.ru>
2020-11-06 01:29:42 +0500

    renamed functions

--- src/formatters/formatterStylish.js
+++ src/formatters/formatterStylish.js
@@ -2,10 +2,10 @@ import _ from 'lodash';
 
 const space = '  ';
 
-const runFormatStylish = (innerTree, spacesCount) => {
+const iter = (innerTree, spacesCount) => {
   const indentBeforeKey = space.repeat(spacesCount);
   const indentBeforeCloseCurlyBrace = space.repeat(spacesCount - 1);
-  const decomposeValue = (value) => {
+  const stringify = (value) => {
     const indentForDecomposition = space.repeat(spacesCount + 1);
     if (_.isObject(value)) {
       return JSON.stringify(value, null, '    ')
@@ -18,27 +18,27 @@ const runFormatStylish = (innerTree, spacesCount) => {
     const openCurlyBrace = (index === 0) ? '{\n' : '';
     const closeCurlyBrace = (index === (array.length - 1)) ? `\n${indentBeforeCloseCurlyBrace}}` : '';
     if (node.type === 'nested') {
-      const stringForNestedValue = runFormatStylish(node.children, spacesCount + 2);
+      const stringForNestedValue = iter(node.children, spacesCount + 2);
       return `${openCurlyBrace}${indentBeforeKey}  ${node.key}: ${stringForNestedValue}${closeCurlyBrace}`;
     }
     if (node.type === 'unchanged') {
-      return `${openCurlyBrace}${indentBeforeKey}  ${node.key}: ${decomposeValue(node.value)}${closeCurlyBrace}`;
+      return `${openCurlyBrace}${indentBeforeKey}  ${node.key}: ${stringify(node.value)}${closeCurlyBrace}`;
     }
     if (node.type === 'changed') {
-      const stringForValue1 = `${indentBeforeKey}- ${node.key}: ${decomposeValue(node.value1)}`;
-      const stringForValue2 = `${indentBeforeKey}+ ${node.key}: ${decomposeValue(node.value2)}`;
+      const stringForValue1 = `${indentBeforeKey}- ${node.key}: ${stringify(node.value1)}`;
+      const stringForValue2 = `${indentBeforeKey}+ ${node.key}: ${stringify(node.value2)}`;
       return `${openCurlyBrace}${stringForValue1}\n${stringForValue2}${closeCurlyBrace}`;
     }
     if (node.type === 'removed') {
-      return `${openCurlyBrace}${indentBeforeKey}- ${node.key}: ${decomposeValue(node.value)}${closeCurlyBrace}`;
+      return `${openCurlyBrace}${indentBeforeKey}- ${node.key}: ${stringify(node.value)}${closeCurlyBrace}`;
     }
-    return `${openCurlyBrace}${indentBeforeKey}+ ${node.key}: ${decomposeValue(node.value)}${closeCurlyBrace}`;
+    return `${openCurlyBrace}${indentBeforeKey}+ ${node.key}: ${stringify(node.value)}${closeCurlyBrace}`;
   });
   return result.join('\n');
 };
 
 const spacesCount = 1;
 
-const formatToStylish = (innerTree) => runFormatStylish(innerTree, spacesCount);
+const formatToStylish = (innerTree) => iter(innerTree, spacesCount);
 
 export default formatToStylish;
