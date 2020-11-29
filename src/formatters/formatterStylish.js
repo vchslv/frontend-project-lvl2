b5738a7c2f4769cc77abefbf6eb2267cef85cb3b
vchslv <kosv2007@yandex.ru>
2020-11-29 20:36:45 +0500

    corrected formatterStylish

--- src/formatters/formatterStylish.js
+++ src/formatters/formatterStylish.js
@@ -1,16 +1,14 @@
 import _ from 'lodash';
 
-const initIndent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
-
-const endIndent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 4);
+const indent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
 
 const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return data;
   }
   const entries = Object.entries(data);
-  const result = entries.map(([key, value]) => (`${initIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`));
-  return `{\n${result.join('\n')}\n${endIndent(depth + 1)}}`;
+  const result = entries.map(([key, value]) => (`${indent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`));
+  return `{\n${result.join('\n')}\n${indent(depth + 1, 2)}${indent(depth + 1, 2)}}`;
 };
 
 const iter = (innerTree, depth = 1) => {
@@ -18,24 +16,24 @@ const iter = (innerTree, depth = 1) => {
     switch (node.type) {
       case 'nested': {
         const stringChildren = iter(node.children, depth + 1);
-        return `${initIndent(depth)}  ${node.key}: ${stringChildren}`;
+        return `${indent(depth)}  ${node.key}: ${stringChildren}`;
       }
       case 'unchanged':
-        return `${initIndent(depth)}  ${node.key}: ${stringify(node.value, depth)}`;
+        return `${indent(depth)}  ${node.key}: ${stringify(node.value, depth)}`;
       case 'changed': {
-        const stringForValue1 = `${initIndent(depth)}- ${node.key}: ${stringify(node.value1, depth)}`;
-        const stringForValue2 = `${initIndent(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
+        const stringForValue1 = `${indent(depth)}- ${node.key}: ${stringify(node.value1, depth)}`;
+        const stringForValue2 = `${indent(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
         return `${stringForValue1}\n${stringForValue2}`;
       }
       case 'removed':
-        return `${initIndent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
+        return `${indent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
       case 'added':
-        return `${initIndent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
+        return `${indent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
       default:
         throw new Error(`Unknown node type '${node.type}'`);
     }
   });
-  return `{\n${result.join('\n')}\n${endIndent(depth)}}`;
+  return `{\n${result.join('\n')}\n${indent(depth, 2)}${indent(depth, 2)}}`;
 };
 
 const formatToStylish = (innerTree) => iter(innerTree);
